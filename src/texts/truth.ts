2a38ee32ce7cf2abe782f543a53426c11b3fd13f
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-02-17 17:47:39 +0500

    Update truth.ts

--- src/texts/truth.ts
+++ src/texts/truth.ts
@@ -4,7 +4,7 @@ export let truthjson: string[] = []
 export default async function truth (): Promise<string> {
   if (!truthjson.length) {
     truthjson = await got(
-      'https://raw.githubusercontent.com/BochilTeam/database/master/kata-kata/truth.json'
+      'https://raw.githubusercontent.com/xIKRATOSx/Ikratos-MD-v1/main/lib/truth.json'
     ).json()
   }
   return truthjson[Math.floor(truthjson.length * Math.random())]
