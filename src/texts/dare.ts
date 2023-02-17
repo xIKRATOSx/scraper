97dc15f0c3552440b2559e019463e4881d7fc2ce
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-02-17 17:46:46 +0500

    Update dare.ts

--- src/texts/dare.ts
+++ src/texts/dare.ts
@@ -3,7 +3,7 @@ export let darejson: string[] = []
 export default async function dare (): Promise<string> {
   if (!darejson.length) {
     darejson = await got(
-      'https://raw.githubusercontent.com/BochilTeam/database/master/kata-kata/dare.json'
+      'https://raw.githubusercontent.com/xIKRATOSx/Ikratos-MD-v1/main/lib/dare.json'
     ).json()
   }
   return darejson[Math.round(darejson.length * Math.random())]
